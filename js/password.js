b0a0e931e02c81517bb5285ad93c09e6703c0355
Lucas Renan <lucasrenandns96@gmail.com>
2022-09-20 14:59:08 -0300

    Update

--- js/password.js
+++ js/password.js
@@ -1,15 +1,17 @@
 const button = document.getElementById("button")
 button.addEventListener("click", generatePassword)
 
+let exit = document.getElementById("exit")
+
 function generatePassword() {
     const characters = "123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&?"
     const maxLengthCharacters = 16
-    let newPassword = "" 
+    let newPassword = ""
       
     for(i = 0; i < maxLengthCharacters; i ++) {
-        const randomPassword = Math.floor(Math.random() * characters.length)
+        let randomPassword = Math.floor(Math.random() * characters.length)
         newPassword += characters.charAt(randomPassword, randomPassword + 1)
     }
     document.getElementById("password").value = newPassword
-    
+    exit.innerHTML = `Sua senha tem <strong>${maxLengthCharacters}</strong> digitos.`
 }
