4aa701ed895ea4cbf505ce3edaecd7085de96627
Lucas Renan <lucasrenandns96@gmail.com>
2022-09-21 10:42:37 -0300

    Update

--- js/password.js
+++ js/password.js
@@ -1,17 +1,34 @@
-const button = document.getElementById("button")
-button.addEventListener("click", generatePassword)
+const generatePass = document.getElementById("generatePass")
+generatePass.addEventListener("click", generatePassword)
 
+const copyPass = document.getElementById("copyPass")
+copyPass.addEventListener("click", copyPassword)
+
+let inputPassword = document.getElementById("password")
 let exit = document.getElementById("exit")
+const characters = "123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&?"
+const maxLengthCharacters = 16
+let newPassword = ""
 
+// Gerar Senha
 function generatePassword() {
-    const characters = "123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&?"
-    const maxLengthCharacters = 16
-    let newPassword = ""
-      
+    
+    newPassword = ""
     for(i = 0; i < maxLengthCharacters; i ++) {
         let randomPassword = Math.floor(Math.random() * characters.length)
         newPassword += characters.charAt(randomPassword, randomPassword + 1)
     }
-    document.getElementById("password").value = newPassword
+    inputPassword.value = newPassword
     exit.innerHTML = `Sua senha tem <strong>${maxLengthCharacters}</strong> digitos.`
+}
+
+// Copiar Senha
+function copyPassword() {
+    if(!inputPassword.value) {
+        window.alert("Gere uma senha para continuar!")
+    }
+    else {
+        navigator.clipboard.writeText(newPassword)
+        window.alert("Senha copiada com sucesso!")
+    }
 }
